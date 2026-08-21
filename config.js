a5ad15de5d78bac65ba77f7b01e9604ca5e2172e
ajithkumar-devops <eajithkumar10@gmail.com>
2026-08-21 20:17:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,28 +5,23 @@ const CONFIG = {
     memories: [
         {
             image: "https://0v16kfqw4dfwdg1i.public.blob.vercel-storage.com/A7C3ECCC-6864-49FF-9940-8C887A4FADDD.png",
-            title: "That little moment",
-            text: "Some moments look ordinary when they happen. Later, you realise they weren't."
+            title: "Then cute, now breathtaking 😍"
         },
         {
             image: "https://0v16kfqw4dfwdg1i.public.blob.vercel-storage.com/IMG_0366.jpeg",
-            title: "The random conversations",
-            text: "Somehow the smallest conversations with you became the ones I remembered."
+            title: "The random conversations"
         },
         {
             image: "https://0v16kfqw4dfwdg1i.public.blob.vercel-storage.com/687CA0B0-32AF-4E19-AEA6-4B2C5FDD0FE3.png",
-            title: "The way you make me smile",
-            text: "You probably don't realise how often you've made a normal day feel a little better."
+            title: "The way you make me smile"
         },
         {
             image: "https://0v16kfqw4dfwdg1i.public.blob.vercel-storage.com/26FDEC81-B021-4B6D-BA2F-4B7C9CAA4812.jpeg",
-            title: "The way you make me smile",
-            text: "You probably don't realise how often you've made a normal day feel a little better."
+            title: "The way you make me smile"
         },
         {
             image: "https://0v16kfqw4dfwdg1i.public.blob.vercel-storage.com/IMG_0373.jpeg",
-            title: "Another little memory",
-            text: "Another moment I wanted to keep here with you. ♡"
+            title: "Another little memory"
         }
     ]
 };
